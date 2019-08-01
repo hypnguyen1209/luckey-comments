e23be2081acd971d74a7282296476a25d22d8ead
Nguyen Van Hiep <hiep12092001@gmail.com>
2019-08-01 23:19:16 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
             var ran = Math.random();
             var luckeyNum = Math.ceil(length * ran);
             var messs = reponse.data[luckeyNum].message;
-            var mess = (messs.length > 60) ? messs.substring(0, 50) + '...' : messs;
+            var mess = (messs.length > 60) ? messs.substring(0, 60) + '...' : messs;
             table.innerHTML += '<tr><td>' + i + '</td><td><div style="margin-right: 10px;" class="flag"><img class="img" src="https://graph.facebook.com/' + reponse.data[luckeyNum].from.id + '/picture?type=large&redirect=true&width=25&height=25"></div></td><td class="description"><a href="https://fb.com/' + reponse.data[luckeyNum].from.id + '" target="_blank">' + reponse.data[luckeyNum].from.name + '</a><td class="description">' + reponse.data[luckeyNum].from.id + '</td></td><td class="description"><a href="https://fb.com/' + reponse.data[luckeyNum].id + '" target="_blank">View Comment</a></td><td class="description">' + mess + '</td></tr>';
             console.log(reponse.data[luckeyNum].from.id);
         }
